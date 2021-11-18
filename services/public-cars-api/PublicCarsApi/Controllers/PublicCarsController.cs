d2ad93a3e71fbcf1a9641a9915e06a19cbb4f645
Mikhail Gorodilov <misha5555573@gmail.com>
2021-11-18 10:50:57 +0000

    Remove real data from production

--- services/public-cars-api/PublicCarsApi/Controllers/PublicCarsController.cs
+++ services/public-cars-api/PublicCarsApi/Controllers/PublicCarsController.cs
@@ -29,11 +29,12 @@ namespace PublicCarsApi.Controllers
 #if DEBUG
             using var streamReader = new StreamReader("defaultResponse.json");
 #else
-            using var httpClient = new HttpClient();
+            using var streamReader = new StreamReader("defaultResponse.json");
+            //using var httpClient = new HttpClient();
 
-            using var response = await httpClient.GetAsync("https://us-central1-carsharinghub.cloudfunctions.net/cachedcars");
-            var contentStream = await response.Content.ReadAsStreamAsync();
-            using var streamReader = new StreamReader(contentStream);
+            //using var response = await httpClient.GetAsync("https://us-central1-carsharinghub.cloudfunctions.net/cachedcars");
+            //var contentStream = await response.Content.ReadAsStreamAsync();
+            //using var streamReader = new StreamReader(contentStream);
 #endif
             using var jsonReader = new JsonTextReader(streamReader);
             var serializer = new JsonSerializer();
@@ -64,11 +65,12 @@ namespace PublicCarsApi.Controllers
 #if DEBUG
             using var streamReader = new StreamReader("defaultResponse.json");
 #else
-            using var httpClient = new HttpClient();
+            using var streamReader = new StreamReader("defaultResponse.json");
+            //using var httpClient = new HttpClient();
 
-            using var response = await httpClient.GetAsync("https://us-central1-carsharinghub.cloudfunctions.net/cachedcars");
-            var contentStream = await response.Content.ReadAsStreamAsync();
-            using var streamReader = new StreamReader(contentStream);
+            //using var response = await httpClient.GetAsync("https://us-central1-carsharinghub.cloudfunctions.net/cachedcars");
+            //var contentStream = await response.Content.ReadAsStreamAsync();
+            //using var streamReader = new StreamReader(contentStream);
 #endif
             using var jsonReader = new JsonTextReader(streamReader);
             var serializer = new JsonSerializer();
